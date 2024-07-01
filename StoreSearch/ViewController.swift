d94e9ce58c99aff3ca5ca1ae716374dc244c10d3
Hlushchuk Dzmitry <glushhukd@gmail.com>
2024-07-01 09:30:34 +0300

    Initial Commit

---
+++ StoreSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by Dzmitry Hlushchuk on 1.07.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
